3851d541b7971bbcc7d7b312ac9510826ca7561a
kbhujbal <kbhujbal@usc.edu>
2025-07-28 15:05:48 -0700

    Adapter

--- Design Patterns/Structural/Adapter/Client/Main.java
+++ Design Patterns/Structural/Adapter/Client/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         WeightMachineAdapter weightMachineAdapter = new WeightMachineAdapterImpl(new WeightMachineForBabie());
+        
         System.out.println(weightMachineAdapter.getWeightInKg());
     }    
 }
