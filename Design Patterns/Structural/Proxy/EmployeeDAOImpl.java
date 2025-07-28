382e57fe4b9a8241844b0a7e72e5460274ce8064
kbhujbal <kbhujbal@usc.edu>
2025-07-28 12:47:39 -0700

    Proxy

--- Design Patterns/Structural/Proxy/EmployeeDAOImpl.java
+++ Design Patterns/Structural/Proxy/EmployeeDAOImpl.java
@@ -1,3 +1,22 @@
 public class EmployeeDAOImpl implements EmployeeDAO{
     
+    @Override
+    public void create(String client, EmployeeDo obj) throws Exception{
+
+        //Creates a new row
+        System.out.println("Created new row in the employee table");
+    }
+
+    @Override
+    public void delete(String client, int employeeId) throws Exception{
+
+        // delete a row
+        System.out.println("deleted a row with employeeID : " + employeeId);
+    }
+
+    @Override
+    public EmployeeDo get(String client, int employeeId) throws Exception{
+        System.out.println("Fetching data from DB");
+        return new EmployeeDo();
+    }
 }
--- Design Patterns/Structural/Proxy/Main.java
+++ Design Patterns/Structural/Proxy/Main.java
@@ -1,3 +1,14 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        
+        try {
+            EmployeeDAO empTableObj = new EmployeeDaoProxy();
+            empTableObj.create("USER", new EmployeeDo());
+            System.out.println("Operation Successful");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
 }
